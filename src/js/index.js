10b8edb76eca481c1844db2d14c80188f6ae8ac7
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-03-23 10:35:07 -0300

    refactor: move for loop inside renderLeads functions

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,6 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el")
 const btn = document.getElementById("input-btn") 
 const ulEl = document.getElementById("ul-el")
-let listItems = ""
 
 
 btn.addEventListener("click", () => { 
@@ -13,17 +12,22 @@ btn.addEventListener("click", () => {
     
 })
 
-for (let i = 0; i < myLeads.length; i++) {
-    let listItems = ""
 
-    listItems += `<li>" ${myLeads[i]} </li>`
 
-    listItems += `    
-        <li>
-            <a href="${myLeads}" target="_blank"> ${myLeads[i]} </a>
-        </li>
-    `
+function renderLeads() {
+    
+    for (let i = 0; i < myLeads.length; i++) {
+        let listItems = ""
+    
+        listItems += `<li>" ${myLeads[i]} </li>`
+    
+        listItems += `    
+            <li>
+                <a href="${myLeads}" target="_blank"> ${myLeads[i]} </a>
+            </li>
+        `
+    
+    }
 
+    ulEl.innerHTML = listItems
 }
-
-ulEl.innerHTML = listItems
